d00e245d018309980aa7617860dfa768154f8abb
Jotis1 <cuellarjotis@gmail.com>
2023-11-15 14:15:22 +0100

    Finish

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -12,148 +12,229 @@ import {
   IonInfiniteScrollContent,
   IonAvatar,
   IonInput,
+  IonPopover,
+  IonImg,
 } from '@ionic/react';
-import axios from 'axios';
 
-import './Tab1.css';
+import { Geolocation } from '@capacitor/geolocation';
 
-const Tab1: React.FC = () => {
-  const [empleados, setEmpleados] = useState([]);
-  const [imagenes, setImagenes] = useState({});
-  const [items, setItems] = useState([]);
-  const [currentPage, setCurrentPage] = useState(0);
-  const [loading, setLoading] = useState(false);
-  const ionInfiniteScrollRef = useRef<HTMLIonInfiniteScrollElement>(null);
-  const [resultadosBusqueda, setResultadosBusqueda] = useState([]);
+const getCurrentPosition = async () => {
+  const coordinates = await Geolocation.getCurrentPosition();
 
-  const fetchData = async () => {
-    try {
-      const usuario = 'crm';
-      const contraseña = '5gtb ygti nVfz O39w tq75 fFjf';
-      const authHeader = `Basic ${btoa(`${usuario}:${contraseña}`)}`;
+  return coordinates.coords;
+};
 
-      const response = await axios.get(`https://empleados.appsbecallgroup.com/wp-json/jet-cct/empleados`, {
-        headers: {
-          Authorization: authHeader,
-        },
-      });
+import { UserProps, UserAPIProps, ImageProps } from "../../../lib/types/index";
 
-      var data = response.data;
-      const requests = Array.from({ length: 30 }, (_, i) =>
-        axios.get(`https://empleados.appsbecallgroup.com/wp-json/wp/v2/media/?author=1&page=${i + 1}`)
-      );
+export default function Tab1() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedUsers, setLoaderUsers] = useState(0);
+  const [users, setUsers] = useState<UserProps[] | []>([]);
+  const [images, setImages] = useState([]);
+  const [renderedUsers, setRenderedUsers] = useState<any>([]);
+  const [searchResults, setSearchResults] = useState([]);
+  const [coordinates, setCoordinates] = useState({ latitude: 0, longitude: 0 });
 
-      var obj = {};
-      const responses = await Promise.all(requests);
+  const usersUrl = "https://empleados.appsbecallgroup.com/wp-json/jet-cct/empleados";
+  const imagesUrl = "https://empleados.appsbecallgroup.com/wp-json/wp/v2/media/?author=1&page="
+  const usuario = 'crm';
+  const contraseña = '5gtb ygti nVfz O39w tq75 fFjf';
+  const authHeader = `Basic ${btoa(`${usuario}:${contraseña}`)}`;
 
-      responses.forEach((response) => {
-        response.data.forEach((e: any) => {
-          obj[e.id] = e.source_url;
+
+  useEffect(() => {
+    getCurrentPosition().then(res => setCoordinates(res));
+    fetch(usersUrl, { method: "GET", cache: "default", headers: { Authorization: authHeader } })
+      .then(userRes => userRes.json())
+      .then((userDoc) => {
+
+        let newUserArray: UserProps | any = [];
+
+        userDoc.forEach((user: UserAPIProps) => {
+          newUserArray.push({
+            id: user._ID,
+            email: user.email,
+            tel: user.telefono,
+            delegation: user.delegacion,
+            name: user.nombre,
+            lastname: user.apellidos,
+            photoID: user.foto,
+            fullname: `${user.nombre} ${user.apellidos}`
+          });
         });
-      });
 
-      setImagenes(obj)
+        setUsers(newUserArray);
 
-      const newItems: any = [];
-      for (let i = 0; i < 50; i++) {
-        newItems.push(data[i]);
+        const imageRequests = Array.from({ length: 30 }, (_, index) =>
+          fetch(imagesUrl + (index + 1), { method: "GET", cache: "default" })
+            .then(imageRes => imageRes.json())
+        );
+
+        Promise.all(imageRequests)
+          .then(imageDocs => {
+            const newImages = imageDocs.reduce((acc, imagesPage) => {
+              imagesPage.forEach((image: ImageProps) => {
+
+                let imageUrl = image?.media_details?.sizes?.thumbnail?.source_url || "https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/User-avatar.svg/1200px-User-avatar.svg.png";
+                acc[image.id] = imageUrl;
+
+              });
+              return acc;
+            }, {});
+
+            setImages(prevImages => ({ ...prevImages, ...newImages }));
+          });
+
+      })
+  }, []);
+
+  const generateUsers = () => {
+    const newUsers: UserAPIProps | any = [];
+    for (let i = loadedUsers; i < loadedUsers + 50; i++) {
+      const userToAdd: any = {
+        id: users[i].id,
+        fullname: users[i].fullname,
+        name: users[i].name,
+        lastname: users[i].lastname,
+        photoID: users[i].photoID,
+        delegation: users[i].delegation,
+        email: users[i].email,
+        tel: users[i].tel,
+      };
+
+      const userAlreadyExists = renderedUsers.some((existingUser: UserProps) => existingUser.id === userToAdd.id);
+
+      if (!userAlreadyExists) {
+        newUsers.push(userToAdd);
       }
-
-      setCurrentPage(50);
-      setItems(data);
-      setEmpleados((prevEmpleados): any => [...prevEmpleados, ...newItems]);
-
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    } finally {
-      setLoading(false);
     }
+    setRenderedUsers([...renderedUsers, ...newUsers]);
+    setLoaderUsers((prev) => prev + 50);
   };
 
-  function buscarPorNombre(array: [], cadenaABuscar: string) {
-
-    const resultados = array.filter(objeto =>
-      objeto.nombre.toLowerCase().includes(cadenaABuscar.toLowerCase()
-      )
+  function searchByName(array: UserProps[], cadenaABuscar: any) {
+    const res: any = array.filter((object: UserProps) =>
+      object.fullname.toLowerCase().includes(cadenaABuscar.toLowerCase())
     );
-
-    setResultadosBusqueda(resultados.length > 0 ? resultados : undefined);
-    console.log(resultadosBusqueda)
+    setSearchResults(res.length > 0 ? res : []);
   }
 
   useEffect(() => {
-    fetchData();
-  }, []);
-
-
-  const loadMoreData = async (ev: any) => {
-    console.log("Fin, página: " + currentPage + ", cargando: " + loading)
-    if (!loading) {
-      setLoading(true);
-      const newItems: any = [];
-      for (let i = currentPage; i < currentPage + 50; i++) {
-        newItems.push(items[i]);
-      };
-      setCurrentPage((prev) => prev + 50);
-      setEmpleados((prevEmpleados): any => [...prevEmpleados, ...newItems]);
-      setLoading(false);
-      ev.target.complete()
-    };
-  }
+    if (images && users.length > 0) {
+      setIsLoading(false);
+      generateUsers();
+    }
+  }, [images]);
 
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Inicio</IonTitle>
+          {coordinates.latitude != 0 && coordinates.longitude != 0 && (
+            <IonTitle size="large">Latitud: {coordinates.latitude.toFixed(2)} Longitud: {coordinates.longitude.toFixed(2)}</IonTitle>
+          )
+          }
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Inicio</IonTitle>
+          <IonToolbar>{ }
+            <IonTitle size="large"></IonTitle>
           </IonToolbar>
         </IonHeader>
-        <IonItem>
-          <IonInput placeholder='Buscar empleado' onIonChange={(e) => buscarPorNombre(empleados, e.detail.value)}></IonInput>
-        </IonItem>
-        <IonList>
-          {resultadosBusqueda.length > 0 ? (
-            <>
-              {resultadosBusqueda.map((empleado, index) => (
-                <IonItem key={index}>
-                  <IonAvatar slot="start">
-                    <img src={imagenes[empleado.foto]} alt="avatar" />
-                  </IonAvatar>
-                  <IonLabel>{empleado.nombre} {empleado.apellidos}</IonLabel>
-                </IonItem>
-              ))}
-            </>
-          ) : (
-            <>
-              {empleados.map((empleado, index) => (
-                <IonItem key={index}>
-                  <IonAvatar slot="start">
-                    <img src={imagenes[empleado.foto]} alt="avatar" />
-                  </IonAvatar>
-                  <IonLabel>{empleado.nombre} {empleado.apellidos}</IonLabel>
-                </IonItem>
-              ))}
-            </>
-          )}
-        </IonList>
+        {isLoading ? (
+          <p>Cargando empleados...</p>
+        ) : (
 
-        <IonInfiniteScroll
-          threshold="100px"
-          disabled={loading}
-          ref={ionInfiniteScrollRef}
-          onIonInfinite={(ev) => loadMoreData(ev)}
-        >
-          <IonInfiniteScrollContent loadingText="Cargando más empleados..." />
-        </IonInfiniteScroll>
+          <>
+            <IonItem>
+              <IonInput placeholder='Buscar empleado' onIonChange={(e) => searchByName(users, e.detail.value)}></IonInput>
+            </IonItem>
+            {searchResults && searchResults.length > 0 ? (
+              <IonList>
+                <>
+                  {searchResults.map((user: any, index) => (
+                    <IonItem key={index} id={`open-popover-${index}`}>
+                      <IonAvatar slot="start">
+                        <img src={images[user.photoID] ? images[user.photoID] : "https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/User-avatar.svg/1200px-User-avatar.svg.png"} alt="avatar" />
+                      </IonAvatar>
+                      <IonLabel>{user.fullname}</IonLabel>
+                      <IonPopover size="cover" trigger={`open-popover-${index}`}>
+                        <IonContent class="ion-padding">
+                          <IonList>
+                            <IonImg style={{ width: "100%", objectFit: "contain" }} slot="start" src={images[user.photoID] ? images[user.photoID] : "https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/User-avatar.svg/1200px-User-avatar.svg.png"}>
+                            </IonImg>
+                            <IonItem>
+                              Nombre: {user.name}
+                            </IonItem>
+                            <IonItem>
+                              Apellido: {user.lastname}
+                            </IonItem>
+                            <IonItem>
+                              Delegación: {user.delegation}
+                            </IonItem>
+                            <IonItem>
+                              Correo: {user.email}
+                            </IonItem>
+                            <IonItem>
+                              Teléfono: {user.tel}
+                            </IonItem>
+                          </IonList>
+                        </IonContent>
+                      </IonPopover>
+                    </IonItem>
+                  ))}
+                </>
+              </IonList>
+            ) : (
+              <IonList>
+                <>
+                  {renderedUsers.map((user: any, index: number) => (
+                    <IonItem key={index} id={`open-popover-${index}`}>
+                      <IonAvatar slot="start">
+                        <img src={images[user.photoID] ? images[user.photoID] : "https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/User-avatar.svg/1200px-User-avatar.svg.png"} alt="avatar" />
+                      </IonAvatar>
+                      <IonLabel>{user.fullname}</IonLabel>
+                      <IonPopover size="cover" trigger={`open-popover-${index}`}>
+                        <IonContent class="ion-padding">
+                          <IonList>
+                            <IonImg style={{ width: "100%", objectFit: "contain" }} slot="start" src={images[user.photoID] ? images[user.photoID] : "https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/User-avatar.svg/1200px-User-avatar.svg.png"}>
+                            </IonImg>
+                            <IonItem>
+                              Nombre: {user.name}
+                            </IonItem>
+                            <IonItem>
+                              Apellido: {user.lastname}
+                            </IonItem>
+                            <IonItem>
+                              Delegación: {user.delegation}
+                            </IonItem>
+                            <IonItem>
+                              Correo: {user.email}
+                            </IonItem>
+                            <IonItem>
+                              Teléfono: {user.tel}
+                            </IonItem>
+                          </IonList>
+                        </IonContent>
+                      </IonPopover>
+                    </IonItem>
+                  ))}
+                </>
+              </IonList>
+            )}
+            <IonInfiniteScroll
+              onIonInfinite={(ev) => {
+                generateUsers();
+                setTimeout(() => ev.target.complete(), 500);
+              }}
+            >
+              <IonInfiniteScrollContent>
+              </IonInfiniteScrollContent>
+            </IonInfiniteScroll>
+          </>
+        )}
       </IonContent>
     </IonPage >
   );
-};
-
-export default Tab1;
+}
--- test.js
+++ test.js
@@ -1,1 +1,5 @@
+import fetch from "node-fetch";
 
+fetch("https://empleados.appsbecallgroup.com/wp-json/wp/v2/media/4710")
+    .then(res => res.json())
+    .then(doc => console.log(doc.media_details.sizes.thumbnail.source_url))
